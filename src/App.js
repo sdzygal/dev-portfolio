dfa2e1ea3a045a72861feef2b6ba479daf8af255
Alexandra Dzygal <sdzygal@gmail.com>
2023-02-24 22:05:08 -0700

    Adding particles to background of homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Home from "./containers/home/Home";
 import About from "./containers/about/About";
 import Skills from "./containers/skills/Skills";
 import Portfolio from "./containers/portfolio/Portfolio";
 import Contact from "./containers/contact/Contact";
 import Navbar from "./components/navbar/Navbar";
+import particles from "./utils/Particles";
 
 function App() {
+
+    const location = useLocation();
+
+    const handleInit = async (main) => {
+        await loadFull(main)
+    }
+
+    const renderParticle = location.pathname === "/";
+
   return (
     <div className="App">
+        {
+            renderParticle &&  <Particles id="particles" options={particles} init={handleInit} />
+        }
+
+
+
         <Navbar />
 
       <Routes>
---
+++ src/utils/Particles.js
@@ -0,0 +1,72 @@
+export default {
+    background: {
+        color: {
+            value: "",
+        },
+    },
+    fpsLimit: 100,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: "push",
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#435acb",
+        },
+        links: {
+            color: "#576ee0",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 4,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 100,
+        },
+        opacity: {
+            value: 0.2,
+        },
+        shape: {
+            type: "circle",
+        },
+        size: {
+            value: { min: 1, max: 5 },
+        },
+    },
+    detectRetina: true,
+}
