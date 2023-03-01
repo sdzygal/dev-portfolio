900cc9dcdfff786b2c9a34dd991c5627d940a9b0
Alexandra Dzygal <sdzygal@gmail.com>
2023-03-01 10:31:10 -0700

    Added scale to sm btns and linked them

--- src/components/socials/SocialMedia.jsx
+++ src/components/socials/SocialMedia.jsx
@@ -7,8 +7,8 @@ const SocialMedia = () => {
     return (
         <div className="socialMedia__container">
             <div className="socialMedia__container-icons">
-            <a href="#"><AiFillLinkedin className="linkedin" /></a>
-            <a href="#"><AiFillGithub className="git" /></a>
+            <a href="https://www.linkedin.com/in/sdzyhal/" rel="noreferrer" target="_blank"><AiFillLinkedin className="linkedin" /></a>
+            <a href="https://github.com/sdzygal" target="_blank"><AiFillGithub className="git" /></a>
             </div>
         </div>
     );
