97d8f7a78f972cdbcffa16c330464ee244cf3b88
Alexandra Dzygal <sdzygal@gmail.com>
2023-03-02 16:59:09 -0700

    Adding projects data

--- src/utils/portfolioData.js
+++ src/utils/portfolioData.js
@@ -1,29 +1,24 @@
-import projectImg from "../assets/2023-01-27 10.27.30.jpg";
-import projectImg2 from "../assets/2023-02-20 12.23.52.jpg";
+import personalWebImg from "../assets/personalWebsite-light.png";
+import ntwistWebImg from "../assets/ntwist-light.png";
+import scaleAppImg from "../assets/scaleApp-light.png";
 
 export const portfolioData = [
     {
         id: 3,
-        name: 'Ecommerce',
-        image: projectImg,
-        link: ''
-    },
-    {
-        id: 2,
-        name: 'Notes App',
-        image: projectImg2,
+        name: 'Company Website',
+        image: ntwistWebImg,
         link: ''
     },
     {
         id: 3,
-        name: 'Website',
-        image: projectImg,
+        name: 'Scale App Clone',
+        image: scaleAppImg,
         link: ''
     },
     {
-        id: 2,
-        name: 'Ecommerce1',
-        image: projectImg,
+        id: 3,
+        name: 'Personal Website',
+        image: personalWebImg,
         link: ''
     }
 ]
