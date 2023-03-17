a0ee5e728fa108431a0189669de8978953c7316a
Alexandra Dzygal <sdzygal@gmail.com>
2023-03-17 14:02:01 -0600

    Cloud refactor in Skills section

--- src/containers/skills/SkillsCanvas.js
+++ src/containers/skills/SkillsCanvas.js
@@ -1,60 +1,66 @@
 import React, { useEffect } from "react";
-import {skillsData} from "../../utils/skillsData";
-import "../../utils/tagcanvas";
+import TagCloud from "TagCloud";
+import "./skills.css";
 
 
 const SkillsCanvas = () => {
     useEffect(() => {
-        console.log('Loading TagCanvas...');
-        const TagCanvas = window.TagCanvas;
-        const tagCanvasOptions = {
-            textColour: '#435acb',
-            outlineThickness: 0.5,
-            outlineColour: 'transparent',
-            maxSpeed: 0.02,
-            freezeActive: true,
-            shape: 'sphere',
-            zoom: 0.8,
-            wheelZoom: false,
-            noSelect: true,
-            textFont: '\'Poppins\', sans-serif',
-            fadeIn: 3000,
-            initial: [0.3, -0.1],
-            depth: 1.5,
+        return () => {
+            const container = ".tagcloud";
+            const texts = [
+                "Git",
+                "JSON",
+                "Railway",
+                "HTML",
+                "React",
+                "REST",
+                "SQL",
+                "MongoSB",
+                "AWS",
+                "Bootstrap",
+                "npm",
+                "CSS",
+                "jQuery",
+                "JavaScript",
+                "Express.js",
+                "Passport.js",
+                "Redux",
+                "Mongoose",
+                "NUX",
+                "Postman",
+                "IDEE",
+                "NodeJS",
+                "Webstorm",
+                "Google Cloud",
+                "Figma",
+                "Illustrator",
+                "Visual Studio Code",
+                "Three.js",
+                "Stripe",
+                "Canvas",
+            ];
+
+            const options = {
+              radius: 280,
+                margin: '-3rem 0 0 -2rem',
+                textFont: '\'Poppins\', sans-serif',
+                depth: 1.5,
+                maxSpeed: "normal",
+                initSpeed: "normal",
+                keep: true,
+            };
+            TagCloud(container, texts, options);
         };
-        try {
-            TagCanvas.Start('tag-canvas', 'tags', tagCanvasOptions);
-        } catch (e) {
-            console.log('Canvas error.');
-            console.log(e);
-        }
     }, []);
 
     return (
+        <>
         <div className='canvas-container'>
-            <canvas
-                id='tag-canvas'
-                width='820'
-                height='600'
-                style={{
-                    maxWidth: '1000px',
-                    width: '100%',
-                    zIndex: '99',
-                    position: 'relative',
-                    margin: '-3rem 0 0 -2rem',
-                }}
-                className='to-fade-in fast-anim'
-            ></canvas>
-            <div id='tags' style={{ display: 'none' }}>
                 <ul>
-                    {skillsData.map(({title, href}) => (
-                        <li key={title}>
-                            <a href={href}>{title}</a>
-                        </li>
-                    ))}
+                    <span className="tagcloud"></span>
                 </ul>
             </div>
-        </div>
+        </>
     );
 };
 
