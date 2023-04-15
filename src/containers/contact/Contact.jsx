148d5aabb61c31543a52ae032fce43ff1203dde2
Alexandra Dzygal <sdzygal@gmail.com>
2023-04-14 18:19:21 -0600

    Contacts page refactoring

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -6,77 +6,34 @@ import { GiSmartphone, GiEnvelope} from "react-icons/gi";
 import Popup from "reactjs-popup";
 import { AnalyticEvent } from "../../utils/google-analytics";
 
+const Contact = () => {
+    const form = useRef();
+    const [modalMessage, setModalMessage] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const closeModal = () => setIsModalOpen(false);
 
+    const openModal = (message) => {
+        setModalMessage(message);
+        setIsModalOpen(true);
+    };
 
+    const sendEmail = async (e) => {
+        e.preventDefault();
 
-    const Contact  = () => {
-        const form = useRef();
-        const [open, setOpen] = useState(false);
-        const [title, setTitle] = useState('success');
-
-        const closeModal = () => setOpen(false);
-
-        function ContactPopup() {
-
-            return (
-                <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                    <div className="modal">
-                        <a href="#" target="_self" className="close" onClick={closeModal}>
-                            &times;
-                        </a>
-                        <div className="message">
-                            <h2>{title}</h2>
-                        </div>
-                    </div>
-                </Popup>
-
-            );
+        try {
+            const result = await emailjs.sendForm('service_7t6as7k', 'template_ql1604d', form.current, 'G0YKxxoi0_SlvtXNQ');
+            AnalyticEvent("Email", "Sent");
+            openModal('Thanks for your email. I will contact you very shortly!');
+        } catch (error) {
+            AnalyticEvent("Email", "Failed");
+            openModal('Something went wrong. Please, try again later');
         }
-
-
-
-
-
-        const sendEmail = (e) => {
-            e.preventDefault();
-
-            emailjs.sendForm('service_7t6as7k', 'template_ql1604d', form.current, 'G0YKxxoi0_SlvtXNQ')
-                .then((result) => {
-                    AnalyticEvent("Email", "Sent");
-                   return (
-                       setOpen(true) & setTitle('Thanks for your email. I will contact you very shortly!')
-                   );
-                }, (error) => {
-                    AnalyticEvent("Email", "Failed");
-                    return (
-                    setOpen(true) &  setTitle('Something went wrong. Please, try again later')
-                    );
-                });
-        };
+    };
 
     return (
         <div className="contact">
-
             <div className="contact__content">
-                <Animate
-                play
-                duration={1}
-                delay={0}
-                start={{
-                    transform: "translateX(-200px)"
-                }}
-                end={{
-                    transform: "translateX(0px)"
-                }}
-               >
-                    <div className="contact__content-header">
-                    <h1>Get in touch</h1>
-                    <h1 className="projectIdea">Have a project?</h1>
-                    </div>
-                </Animate>
-
-                <div className="contact__body">
                 <Animate
                     play
                     duration={1}
@@ -88,38 +45,57 @@ import { AnalyticEvent } from "../../utils/google-analytics";
                         transform: "translateX(0px)"
                     }}
                 >
-                      <ContactPopup />
-                    <form ref={form} onSubmit={sendEmail} className="contact__content-form">
-                        <div className="contact__content-formWrapper">
-                            <input required name="name" className="inputName" type="text" placeholder="Your name"  />
-                            <input required name="email" className="inputEmail" type="text" placeholder="Your email"  />
-                        </div>
-                            <textarea name="project" className="inputProject" type="text" rows="0" placeholder="Project details" />
-                        <label htmlFor="project" className="projectLabel">
-                            <ul>
-                                <li>
-                                    Your project
-                                </li>
-                                <li>
-                                    Current goals
-                                </li>
-                                <li>
-                                    Success criteria
-                                </li>
-                                <li>
-                                    Your budget
-                                </li>
-                            </ul>
-                        </label>
-
-                        <div className="newDate">
-                        <label htmlFor="date" className="startDateLabel">
-                            When would you like to get started?
-                        </label>
-                        <input name="startDate" className="inputStartDate" type="date" placeholder="Select date..." />
+                    <div className="contact__content-header">
+                        <h1>Get in touch</h1>
+                        <h1 className="projectIdea">Have a project?</h1>
                     </div>
-                     <button type="submit">Submit</button>
-                    </form> </Animate>
+                </Animate>
+                <div className="contact__body">
+                    <Animate
+                        play
+                        duration={1}
+                        delay={0}
+                        start={{
+                            transform: "translateX(-200px)"
+                        }}
+                        end={{
+                            transform: "translateX(0px)"
+                        }}
+                    >
+                        <Popup open={isModalOpen} closeOnDocumentClick onClose={closeModal}>
+                            <div className="modal">
+                                <a href="#" target="_self" className="close" onClick={closeModal}>
+                                    &times;
+                                </a>
+                                <div className="message">
+                                    <h2>{modalMessage}</h2>
+                                </div>
+                            </div>
+                        </Popup>
+                        <form ref={form} onSubmit={sendEmail} className="contact__content-form">
+                            <div className="contact__content-formWrapper">
+                                <input required name="name" className="inputName" type="text" placeholder="Your name"  />
+                                <input required name="email" className="inputEmail" type="text" placeholder="Your email"  />
+                            </div>
+                            <textarea name="project" className="inputProject" type="text" rows="0" placeholder="Project details" />
+                            <label htmlFor="project" className="projectLabel">
+                                <ul>
+                                    <li>Your project</li>
+                                    <li>Current goals</li>
+                                    <li>Success criteria</li>
+                                    <li>Your budget</li>
+                                </ul>
+                            </label>
+                            <div className="newDate">
+                                <label htmlFor="date" className="startDateLabel">
+                                    When would you like to get started?
+                                </label>
+                                <input name="startDate" className="inputStartDate" type="date" placeholder="Select date..." />
+                            </div>
+                            <button type="submit">Submit</button>
+                    </form>
+                    </Animate>
+
                     <div className="contact__content-form_rightSide">
                         <h2>Tell me about your vision:</h2>
                         <ul>
